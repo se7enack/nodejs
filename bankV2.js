b806af4d8246a8ab52b50f2aa14cb346a0fb1e01
Stephen Burke <sburke@one.verizon.com>
2016-08-19 21:01:32 -0400

    new function getAccount for Bank v2

--- bankV2.js
+++ bankV2.js
@@ -8,12 +8,23 @@ function createAccount (x,y,z) {
 	account.balance=y;
 	account.number=z;
 	accounts.push(account)
+	console.log(account)
+}
+
+function getAccount (userName) {
+	accounts.forEach(function (account) {
+		if (account.userName === userName) {
+			console.log(account);
+		}
+	}); 
 }
 
 createAccount('Bryson',1999234.01,0);
 createAccount('Logan',2300022,1);
 createAccount('Michelle',14432.88,2);
 createAccount('Stephen',.01,3);
+console.log('\n')
+getAccount("Michelle");
 
 //console.log(accounts[1])
-console.log(accounts)
+//console.log(accounts)
