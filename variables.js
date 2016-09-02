bdafe142f2f102733b8055db57c2d7dc81b28b21
Stephen Burke <se7enack@users.noreply.github.com>
2016-09-02 11:22:48 -0400

    Delete variables.js

--- variables.js
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/node
-
-var lastName = 'Burke';
-var firstName = 'Stephen';
-var fullName = firstName+' '+lastName
-console.log(fullName + "\'s name has " + (fullName.length-1) +" letters in it");
